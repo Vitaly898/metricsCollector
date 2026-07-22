89b09bc9606967859561535f2ad43111374ee044
vepetrov <vepetrov@avito.ru>
2026-07-22 23:28:20 +0300

    написал тесты на хендлер и хранилище

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,11 +6,10 @@ import (
 	"github.com/Vitaly898/metricsCollector/internal/agent"
 )
 
-
-func main(){
+func main() {
 	collector := agent.NewCollector()
 	sender := agent.NewSender("http://localhost:8080")
-	a := agent.NewAgent(collector,sender,2*time.Second,10*time.Second)
+	a := agent.NewAgent(collector, sender, 2*time.Second, 10*time.Second)
 	a.Run()
 
 }
--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -4,30 +4,52 @@ import (
 	"testing"
 )
 
-func TestUpdateStorage(t *testing.T){
+func TestUpdateGauge(t *testing.T) {
 	tests := []struct {
-		name string
+		name   string
 		values []float64
-		want float64
+		want   float64
 	}{
-		{name: "позитивный кейс- обновление метрики один вызов",values:[]float64{3.1}, want: 3.1 },
-		{name: "позитивный кейс- обновление метрики два вызова",values: []float64{3.1, 5.5},want:5.5 },
-		{name: "позитивный кейс- обновление метрики два вызова",values: []float64{0},want:0 },
-		{name: "позитивный кейс- обновление метрики два вызова",values: []float64{-100.0},want:-100 },
-
-
+		{name: "позитивный кейс- обновление метрики один вызов", values: []float64{3.1}, want: 3.1},
+		{name: "позитивный кейс- обновление метрики два вызова", values: []float64{3.1, 5.5}, want: 5.5},
+		{name: "позитивный кейс- обновление метрики два вызова", values: []float64{0}, want: 0},
+		{name: "позитивный кейс- обновление метрики два вызова", values: []float64{-100.0}, want: -100},
 	}
 
-	for _,tt := range tests {
-		t.Run(tt.name,func(t *testing.T){
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			s := NewMemStorage()
-			for _,v := range tt.values{
-				s.UpdateGauge("Alloc",v)
+			for _, v := range tt.values {
+				s.UpdateGauge("Alloc", v)
 			}
-			if got:= s.gauge["Alloc"]; got !=tt.want{
-				t.Errorf("Тест упал %v не равен %v", got,tt.want)
+			if got := s.gauge["Alloc"]; got != tt.want {
+				t.Errorf("Тест упал %v не равен %v", got, tt.want)
 			}
 		})
 	}
 
+}
+
+func TestUpdateCounter(t *testing.T) {
+	tests := []struct {
+		name   string
+		values []int64
+		want   int64
+	}{
+		{name: "позитивный кейс- обновление метрики один вызов", values: []int64{3}, want: 3},
+		{name: "позитивный кейс- обновление метрики числа суммируются", values: []int64{3, 1}, want: 4},
+		{name: "позитивный кейс- обновление метрики отрицательное значение метрики", values: []int64{3, 1, -2}, want: 2},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewMemStorage()
+			for _, v := range tt.values {
+				s.UpdateCounter("PollCount", v)
+			}
+			if got := s.counter["PollCount"]; got != tt.want {
+				t.Errorf("Тест упал счетчик метрики не верный")
+			}
+		})
+	}
 }
